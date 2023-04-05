87849fae6e3152f27cca3110b2e2c89e2a6b9491
jujubusch <129890156+jujubusch@users.noreply.github.com>
2023-04-05 12:19:31 -0300

    areas de divisao

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ document.getElementById( "botao" ).addEventListener( "click" , exibenome );
 
 document.getElementById( "btn-soma" ).addEventListener( "click" , exibesoma );
 
+document.getElementById( "somar" ).addEventListener( "click" , exibirdivisao );
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -20,6 +22,14 @@ function exibesoma()
 {
     let numero1 = document.getElementById( "numero1" ).value;
     let numero2 = document.getElementById( "numero2" ).value;
-    document.getElementById( "resultado1" ).innerHTML = numero1 + parseFloat(numero2);
+    document.getElementById( "resultado1" ).innerHTML = parseFloat(numero1) + parseFloat(numero2);
+
+}
+
+function exibirdivisao()
+{
+    let numero3 = document.getElementById( "numero3" ).value;
+    let numero4 = document.getElementById( "numero4" ).value;
+    document.getElementById( "resultado2" ).innerHTML = parseFloat(numero3) + parseFloat(numero4);
 
 }
