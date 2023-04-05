23f393faa0e7d5c1a08fbd7ce48853e0ee6db89f
jujubusch <129890156+jujubusch@users.noreply.github.com>
2023-04-05 11:26:40 -0300

    alteracao da caixa de texto

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 document.addEventListener( "click" , showcolor );
 
+document.getElementById( "botao" ).addEventListener( "click" , exibenome );
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
 }
 
+function exibenome()
+{
+    let usuario = document.getElementById( "nome" ).value;
+    document.getElementById( "mensagem" ).innerText = "Meu Nome É: " + usuario;
+    document.getElementById( "nome" ).value = "";
+}
